4950956da893847465cc2d43af5e4ef64824b996
Peace102282 <shashankkiranpatil@gmail.com>
2023-10-30 09:18:06 +0530

    30_10

--- problem_data_encp.cpp
+++ problem_data_encp.cpp
@@ -1,1 +1,28 @@
 // get and show any intger
+
+#include <iostream>
+using namespace std;
+
+class I
+{
+private:
+    int integer;
+
+public:
+    int setinteger(int i)
+    {
+        integer = i;
+    }
+    int getinteger()
+    {
+        return integer;
+    }
+};
+
+int main()
+{
+    I i;
+    i.setinteger(10);
+    cout << "employe sallary=" << i.getinteger() << endl;
+    return 0;
+}
